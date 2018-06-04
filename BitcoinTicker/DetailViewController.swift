d41e823b8f9040cb065af944ba58875650570e2d
s2h <ssh92kr0410@naver.com>
2018-06-05 03:08:23 +0900

    json Parsing 수정

--- BitcoinTicker/DetailViewController.swift
+++ BitcoinTicker/DetailViewController.swift
@@ -10,45 +10,22 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-
-
 class DetailViewController: UIViewController {
-    
-    let bitCoinURL = "https://api.bithumb.com/public/ticker/"
-    
-//    let bitCoinArray = ["BTC", "ETH", "DASH", "LTC", "ETC", "XRP", "BCH", "XMR", "ZEC", "QTUM", "BTG", "EOS", "ICX", "VEN", "TRX", "ELF", "MITH", "MCO", "OMG", "KNC", "GNT", "HSR", "ZIL", "ETHOS", "PAY", "WAX", "POWR", "LRC", "GTO", "STEEM", "STRAT"]
-    
-    //let detailArray = ["BTC", "ETH", "DASH", "LTC", "ETC", "XRP", "BCH", "XMR", "ZEC", "QTUM", "BTG", "EOS", "ICX", "VEN", "TRX", "ELF", "MITH", "MCO", "OMG", "KNC", "GNT", "HSR", "ZIL", "ETHOS", "PAY", "WAX", "POWR", "LRC", "GTO", "STEEM", "STRAT"]
-
-    
-        let indexArray = ["최고가", "최저가"]
-    
-    var BTCURL = "https://api.bithumb.com/public/ticker/BTC"
-    var ETHURL = "https://api.bithumb.com/public/ticker/ETH"
-   
-    
-    //let data = currencyData()
     
     @IBOutlet weak var coinIcon: UIImageView!
     @IBOutlet weak var coinName: UILabel!
-    
-    @IBOutlet weak var detailPicker: UIPickerView!
-    
     @IBOutlet weak var maxLabel: UILabel!
-    
     @IBOutlet weak var minLabel: UILabel!
+    @IBOutlet weak var avgLabel: UILabel!
+    @IBOutlet weak var ratePriceLabel: UILabel!
     
-    
+    let bitCoinURL = "https://api.bithumb.com/public/ticker/"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
-
         coinName.text = coinData[myIndex]
         coinIcon.image = UIImage(named: coinData[myIndex])
-        
-        //finalURL = "https://api.bithumb.com/public/ticker/DASH"
         
         getMaxBitcoinData(url: bitCoinURL + coinData[myIndex])
         getMinBitcoinData(url: bitCoinURL + coinData[myIndex])
@@ -63,7 +40,6 @@ class DetailViewController: UIViewController {
     
 //    MARK: - Networking
 //    *************************************************************
-
     func getMaxBitcoinData(url: String) {
 
         Alamofire.request(url, method: .get).responseJSON { (response) in
@@ -74,8 +50,7 @@ class DetailViewController: UIViewController {
                 let bitcoinJSON : JSON = JSON(response.result.value as Any)
 
                 self.maxBitCoinData(json: bitcoinJSON)
-               
-
+                
                 //print(bitcoinJSON)
 
             } else {
@@ -84,7 +59,7 @@ class DetailViewController: UIViewController {
             }
         }
     }
-    
+
     func getMinBitcoinData(url: String) {
         
         Alamofire.request(url, method: .get).responseJSON { (response) in
@@ -93,7 +68,6 @@ class DetailViewController: UIViewController {
                 print("Success")
                 
                 let bitcoinJSON : JSON = JSON(response.result.value as Any)
-                
                 
                 self.minBitCoinData(json: bitcoinJSON)
                 
@@ -110,23 +84,24 @@ class DetailViewController: UIViewController {
     //MARK: - JSON Parsing
 
     func maxBitCoinData(json : JSON) {
-
-        let bitcoinResult = json["data"]["max_price"].intValue
-
-       maxLabel.text = String(bitcoinResult)
-
+        let maxData = json["data"]["max_price"].intValue
+        maxLabel.text = String(maxData) + " 원"
     }
 
     func minBitCoinData(json : JSON) {
-
-        let bitcoinResult2 = json["data"]["min_price"].intValue
-
-        minLabel.text = String(bitcoinResult2)
-
+        let minData = json["data"]["min_price"].intValue
+        minLabel.text = String(minData) + " 원"
     }
     
-
+    func avgBitCoinData(json : JSON) {
+        let avgData = json["data"]["average_price"].intValue
+        avgLabel.text = String(avgData)
+    }
     
+    func ratePriceBitCoinData(json : JSON) {
+        let ratePriceData = json["data"]["24H_fluctate"].intValue
+        ratePriceLabel.text = String(ratePriceData)
+    }
 }
 
 
--- BitcoinTicker/MainViewController.swift
+++ BitcoinTicker/MainViewController.swift
@@ -17,26 +17,13 @@ var myIndex = 0
 
 class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-//    let bitCoinURL = "https://api.bithumb.com/public/ticker/"
-//    let bitCoinArray = ["BTC", "ETH", "DASH", "LTC", "ETC", "XRP", "BCH", "XMR", "ZEC", "QTUM", "BTG", "EOS", "ICX", "VEN", "TRX", "ELF", "MITH", "MCO", "OMG", "KNC", "GNT", "HSR", "ZIL", "ETHOS", "PAY", "WAX", "POWR", "LRC", "GTO", "STEEM", "STRAT"]
-    //var finalURL = ""
-    
-    var BTCURL = "https://api.bithumb.com/public/ticker/BTC"
-    var ETHURL = "https://api.bithumb.com/public/ticker/ETH"
-  
-   
     @IBOutlet weak var tableView: UITableView!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
-//        tableView.reloadData()
-        
-//        getBitcoinData(url: ETHURL)
-        
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -49,8 +36,6 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell") as! CustomTableViewCell
         cell.coinName.text = coinData[indexPath.row]
         cell.iconImage.image = UIImage(named: coinData[indexPath.row])
@@ -62,93 +47,9 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         myIndex = indexPath.row
-//        performSegue(withIdentifier: "goToDetailVC", sender: self)
-//
-//        finalURL = bitCoinURL + bitCoinArray[indexPath.row]
-//        print(finalURL)
-//        getBitcoinData(url: finalURL)
-        
-        
-//        var selectedRow = coinData[indexPath.row]
         performSegue(withIdentifier: "goToDetailVC", sender: self)
         
-//        if selectedRow == coinData[0] {
-//            getBitcoinData(url: BTCURL)
-//            performSegue(withIdentifier: "goToDetailVC", sender: self)
-//            print(BTCURL)
-//
-//        } else if selectedRow == coinData[1] {
-//            getBitcoinData(url: ETHURL)
-//             performSegue(withIdentifier: "goToDetailVC", sender: self)
-//            print(self.ETHURL)
-//
-//        }
-        
-        
-        
-        
-        
-        
-        
     }
-    
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        if let destination = segue.destination as? DetailViewController {
-////            destination.data = data[(tableView.indexPathForSelectedRow?.row)!]
-//            destination.BTCURL = BTCURL
-//            print("asd")
-//            destination.ETHURL = ETHURL
-//        }
-//    }
-
-//
-//
-//
-//
-//    func getBitcoinData(url: String) {
-//
-//        Alamofire.request(url, method: .get).responseJSON { (response) in
-//            if response.result.isSuccess {
-//
-//                print("Success")
-//
-//                let bitcoinJSON : JSON = JSON(response.result.value as Any)
-//
-//                self.updateBitcoinData(json: bitcoinJSON)
-//                self.updateClosingPriceData(json: bitcoinJSON)
-//
-//
-//
-//                //print(bitcoinJSON)
-//
-//            } else {
-//                print("Error: \(String(describing: response.result.value))")
-//
-//            }
-//        }
-//    }
-//
-//    /***************************************************************/
-//    //MARK: - JSON Parsing
-//
-//    func updateBitcoinData(json : JSON) {
-//
-//        let bitcoinResult = json["data"]["opening_price"].intValue
-//
-//        //currencyLabel.text = String(bitcoinResult)
-//
-//    }
-//
-//    func updateClosingPriceData(json : JSON) {
-//
-//        let bitcoinResult2 = json["data"]["closing_price"].intValue
-//
-//        //priceLabel.text = String(bitcoinResult2)
-//
-//    }
-
-
-
 }
 
 
